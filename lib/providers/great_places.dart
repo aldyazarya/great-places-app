78954af432e3cfb41a55bc6b9fc7c0df2bcd8d6c
aldyazarya <aldy1612@gmail.com>
2020-01-04 04:26:58 +0800

    adding Add Place Screen and Image Input Widget

---
+++ lib/providers/great_places.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../models/place.dart';
+
+class GreatPlaces with ChangeNotifier {
+  List<Place> _items = [];
+
+  List<Place> get items {
+    return [..._items];
+  }
+
+  
+
+
+
+}
